ebc4bdd8c79d7681ef6b6fe771a657ba4b498fc5
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2018-12-16 13:49:26 -0600

    linked and tested js file for positive result

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,11 @@
+console.log("Hello World");
+
+
 var computerNumber = "";
 var crystalOne = 1-3;
 var crystalTwo = 4-6;
 var crystalThree = 7-9;
 var crystalFour = 10-12;
 var usersNumber = "";
+
+$(cyrstalOne)
