907e722a510ab46ec93b15a349c6463dc676737b
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2018-12-16 19:53:55 -0600

    divs created in js file and outlined in red in css file to denote where they are

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,20 @@
 //console.log("Hello World");
 
-
-//Better pseudocoding
+//Start with Computer Generated random number from between 19-120.
 //Need to create 4 crystals that each contains a random number from 1-12
-//That number generates for every single new game (Win or Loss)
-//When any crystal is clicked it should add to the previous crystal that was clicked until it meets the Computer generated number or it goes over the Computer generated number
-//
+//The number generates for every single new game (Win or Loss) for each crystal
+//When any crystal is clicked it should add to the previous crystal that was clicked
+//The total of the amount of crystals needs to equal the Computer Generated random number for a Win, added to a counter
+//If the total of the amount of crystals goes over the Computer Generated random number, user gets a Loss, added to a counter
+//When Win or Loss is determined, game restarts with a new Computer Generated number
 
-
-
-var computerNumber = "";
-var crystalOne = 1-12;
-var crystalTwo = 1-12;
-var crystalThree = 1-12;
-var crystalFour = 1-12;
-var usersNumber = "";
-var win;
+var random;
 var loss;
+var win;
+//the below creats 4 divs to hold our crystals and assigns it a css class
+for(var i = 0; i< 4; i++){
 
-$(cyrstalOne)
+    var crystal = $("<div>");
+        crystal.attr("class", 'crystal');
+    $(".crystals").append(crystal);
+}
