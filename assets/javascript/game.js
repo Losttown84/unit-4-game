319891e96643ed684e848b4a44a5d665c63e2099
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2018-12-16 19:28:25 -0600

    new pseudocode and redo of js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,21 @@
-console.log("Hello World");
+//console.log("Hello World");
+
+
+//Better pseudocoding
+//Need to create 4 crystals that each contains a random number from 1-12
+//That number generates for every single new game (Win or Loss)
+//When any crystal is clicked it should add to the previous crystal that was clicked until it meets the Computer generated number or it goes over the Computer generated number
+//
+
 
 
 var computerNumber = "";
-var crystalOne = 1-3;
-var crystalTwo = 4-6;
-var crystalThree = 7-9;
-var crystalFour = 10-12;
+var crystalOne = 1-12;
+var crystalTwo = 1-12;
+var crystalThree = 1-12;
+var crystalFour = 1-12;
 var usersNumber = "";
+var win;
+var loss;
 
 $(cyrstalOne)
